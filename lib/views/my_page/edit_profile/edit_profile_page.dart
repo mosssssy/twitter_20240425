da49ffe8a5fe6ef8db07d8777648b35fcc2e9b84
gonmura <user@Gonmuras-MacBook-Pro.local>
2024-04-26 11:59:28 +0900

    プロフィール編集ページ完成

--- lib/views/my_page/edit_profile/edit_profile_page.dart
+++ lib/views/my_page/edit_profile/edit_profile_page.dart
@@ -1,10 +1,225 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class EditProfilePage extends StatelessWidget {
-  const EditProfilePage({super.key});
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:twitter_20240425/common_widget/close_only_dialog.dart';
+import 'package:twitter_20240425/common_widget/margin_sizedbox.dart';
+import 'package:twitter_20240425/functions/global_functions.dart';
+import 'package:twitter_20240425/views/my_page/components/blue_button.dart';
+
+class EditProfilePage extends StatefulWidget {
+  EditProfilePage({
+    super.key,
+    required this.userName,
+    required this.imageUrl,
+  });
+  final String userName;
+  String imageUrl;
+
+  @override
+  State<EditProfilePage> createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController userNameController = TextEditingController();
+  final TextEditingController profileIntroductionController =
+      TextEditingController();
+  final User user = FirebaseAuth.instance.currentUser!;
+  File? image; // 画像を入れる変数
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    userNameController.text = widget.userName;
+    Widget previewWidget;
+    if (image != null) {
+      previewWidget = ClipOval(
+        child: Image.file(
+          image!,
+          width: 200,
+          height: 200,
+          fit: BoxFit.cover,
+        ),
+      );
+    } else {
+      if (widget.imageUrl != '') {
+        // imageUrlが空文字のとき
+        previewWidget = ClipOval(
+          child: Image.network(
+            widget.imageUrl,
+            width: 200,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+        );
+      } else {
+        previewWidget = ClipOval(
+            child: Image.asset(
+          'assets/images/default_user_icon.png',
+          width: 200,
+          height: 200,
+          fit: BoxFit.cover,
+        ));
+      }
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'プロフィール編集',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Form(
+                key: formKey,
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Stack(
+                        children: [
+                          previewWidget,
+                          if (widget.imageUrl != '')
+                            Positioned(
+                              top: 0,
+                              right: 0,
+                              child: CircleAvatar(
+                                backgroundColor: Colors.grey, // 背景色を設定
+                                child: IconButton(
+                                  icon: const Icon(
+                                    Icons.close,
+                                    color: Colors.black,
+                                  ), // アイコンを設定
+                                  onPressed: () async {
+                                    // ボタンがタップされたときの処理
+                                    // 画像削除
+                                    // 上記で取得したURLを使ってUserドキュメントを更新する
+                                    await FirebaseFirestore.instance
+                                        .collection('users')
+                                        .doc(user.uid)
+                                        .update(
+                                      {
+                                        'imageUrl': '',
+                                        'updatedAt': Timestamp.now(),
+                                      },
+                                    );
+                                    await FirebaseStorage.instance
+                                        .ref('userIcon/${user.uid}')
+                                        .delete();
+                                    widget.imageUrl = '';
+                                    showToast('画像削除しました');
+                                    setState(() {});
+                                  },
+                                ),
+                              ),
+                            ),
+                        ],
+                      ),
+                      MarginSizedBox.mediumHeightMargin,
+                      BlueButton(
+                        buttonText: '画像を選択する',
+                        onBlueButtonPressed: () {
+                          //Image Pickerをインスタンス化
+                          getImageFromGallery();
+                        },
+                      ),
+                      MarginSizedBox.mediumHeightMargin,
+                      TextFormField(
+                          controller: userNameController,
+                          maxLength: 12,
+                          validator: (value) {
+                            if (value == null || value == '') {
+                              return '未入力です';
+                            }
+                            return null;
+                          },
+                          decoration:
+                              const InputDecoration(label: Text('ユーザーネーム'))),
+                      MarginSizedBox.bigHeightMargin,
+                      TextFormField(
+                          controller: profileIntroductionController,
+                          validator: (value) {
+                            if (value == null || value == '') {
+                              return '未入力です';
+                            }
+                            return null;
+                          },
+                          decoration:
+                              const InputDecoration(label: Text('プロフィール文章'))),
+                      BlueButton(
+                        buttonText: 'プロフィールを変更する',
+                        onBlueButtonPressed: () async {
+                          if (formKey.currentState!.validate() == false) {
+                            return;
+                          }
+                          try {
+                            if (image != null) {
+                              ///imageがnullじゃない
+                              ///画像を選択したとき
+                              ///ストレージに選択した画像をアップロードする
+                              final storedImage = await FirebaseStorage.instance
+                                  .ref('userIcon/${user.uid}')
+                                  .putFile(image!);
+                              //ストレージにあげた画像のURLを取得する
+                              final String imageUrl =
+                                  await storedImage.ref.getDownloadURL();
+                              //上記で取得したURLを使ってUserドキュメントを更新する
+                              await FirebaseFirestore.instance
+                                  .collection('users')
+                                  .doc(user.uid)
+                                  .update(
+                                {
+                                  'imageUrl': imageUrl,
+                                  'userName': userNameController.text,
+                                  'profileIntroduction':
+                                      profileIntroductionController.text,
+                                  'updatedAt': Timestamp.now(),
+                                },
+                              );
+                            } else {
+                              ///imageがnull
+                              ///画像を選択していないとき
+                              await FirebaseFirestore.instance
+                                  .collection('users')
+                                  .doc(user.uid)
+                                  .update(
+                                {
+                                  'userName': userNameController.text,
+                                  'profileIntroduction':
+                                      profileIntroductionController.text,
+                                  'updatedAt': Timestamp.now(),
+                                },
+                              );
+                            }
+                            showToast('変更成功しました！');
+                          } catch (error) {
+                            showCloseOnlyDialog(
+                                context, error.toString(), '更新失敗しました');
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ))));
+  }
+
+  Future getImageFromGallery() async {
+    final picker = ImagePicker();
+    final pickedFile =
+        await picker.pickImage(source: ImageSource.gallery); //アルバムから画像を取得
+
+    if (pickedFile != null) {
+      setState(() {
+        image = File(pickedFile.path);
+      });
+    }
   }
 }
--- lib/views/my_page/my_page.dart
+++ lib/views/my_page/my_page.dart
@@ -17,145 +17,169 @@ class MyPage extends StatelessWidget {
     final String? myUserEmail = FirebaseAuth.instance.currentUser!.email;
     String? myUserId = FirebaseAuth.instance.currentUser!.uid;
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'PROFILE',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          backgroundColor: Colors.deepPurple,
-          actions: [
-            IconButton(
-                onPressed: () async {
-                  await FirebaseAuth.instance.signOut();
-                  print('ログアウトしました');
-                },
-                icon: const Icon(
-                  Icons.logout,
-                  color: Colors.white,
-                ))
-          ],
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              MarginSizedBox.bigHeightMargin,
-              ListTile(
-                title: const Text('メールアドレス変更'),
-                onTap: () {
-                  Navigator.pop(context); // Drawerを閉じる
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const EditEmailPage()),
-                  );
-                },
-              ),
-              ListTile(
-                title: const Text('パスワード変更'),
-                onTap: () async {
-                  showConfirmDialog(
-                    context: context,
-                    onPressed: () async {
-                      try {
-                        await FirebaseAuth.instance
-                            .sendPasswordResetEmail(email: myUserEmail!);
-                        showToast("パスワードリセット用のメールを送信しました");
-                        // ignore: use_build_context_synchronously
-                        Navigator.pop(context);
-                      } catch (e) {
-                        showCloseOnlyDialog(context, e.toString(), 'メール送信失敗');
-                      }
-                    },
-                    text: 'パスワード再設定メールを送信しますか？',
-                  );
-                },
-              ),
-              ListTile(
-                title: const Text('プロフィール編集'),
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return const EditProfilePage(
-                        // userName: userData.userName,
-                        // imageUrl: userData.imageUrl,
-                        );
-                    // 2) 実際に表示するページを指定する
-                  }));
-                },
-              ),
-              ListTile(
-                title: const Text('ログアウト'),
-                onTap: () async {
-                  await FirebaseAuth.instance.signOut();
-                  print('ログアウトしました');
-                },
-              ),
-            ],
+      appBar: AppBar(
+        title: const Text(
+          'PROFILE',
+          style: TextStyle(
+            color: Colors.white,
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: SizedBox(
-            width: double.infinity,
-            child: StreamBuilder<Object>(
-                stream: FirebaseFirestore.instance
-                    .collection('users')
-                    .doc(myUserId)
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const SizedBox.shrink();
-                  }
-                  if (snapshot.hasData == false) {
-                    return const SizedBox.shrink();
-                  }
-                  final DocumentSnapshot<Map<String, dynamic>>?
-                      documentSnapshot =
-                      snapshot.data as DocumentSnapshot<Map<String, dynamic>>?;
-                  if (documentSnapshot == null || !documentSnapshot.exists) {
-                    // ドキュメントが存在しない場合の処理
-                    return const SizedBox.shrink();
-                  }
-                  final Map<String, dynamic> userDataMap =
-                      documentSnapshot.data()!;
-                  final UserData userData = UserData.fromJson(userDataMap);
+        backgroundColor: Colors.deepPurple,
+        actions: [
+          IconButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                print('ログアウトしました');
+              },
+              icon: const Icon(
+                Icons.logout,
+                color: Colors.white,
+              ))
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: SizedBox(
+          width: double.infinity,
+          child: StreamBuilder<Object>(
+              stream: FirebaseFirestore.instance
+                  .collection('users')
+                  .doc(myUserId)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const SizedBox.shrink();
+                }
+                if (snapshot.hasData == false) {
+                  return const SizedBox.shrink();
+                }
+                final DocumentSnapshot<Map<String, dynamic>>? documentSnapshot =
+                    snapshot.data as DocumentSnapshot<Map<String, dynamic>>?;
+                if (documentSnapshot == null || !documentSnapshot.exists) {
+                  // ドキュメントが存在しない場合の処理
+                  return const SizedBox.shrink();
+                }
+                final Map<String, dynamic> userDataMap =
+                    documentSnapshot.data()!;
+                final UserData userData = UserData.fromJson(userDataMap);
 
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      if (userData.imageUrl != '')
-                        ClipOval(
-                          child: Image.asset(
-                            userData.imageUrl,
-                            width: 200,
-                            height: 200,
-                            fit: BoxFit.cover,
-                          ),
-                        )
-                      else
-                        ClipOval(
-                          child: Image.asset(
-                            'assets/images/default_user_icon.png',
-                            width: 200,
-                            height: 200,
-                            fit: BoxFit.cover,
-                          ),
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    if (userData.imageUrl != '')
+                      ClipOval(
+                        child: Image.network(
+                          userData.imageUrl,
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
                         ),
-                      Text(
-                        userData.userName,
-                        style: const TextStyle(),
+                      )
+                    else
+                      ClipOval(
+                        child: Image.asset(
+                          'assets/images/default_user_icon.png',
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                      MarginSizedBox.smallHeightMargin,
-                      Text(myUserEmail ?? ''),
-                      MarginSizedBox.smallHeightMargin,
-                      Text(userData.profileIntroduction),
-                      MarginSizedBox.bigHeightMargin,
-                    ],
-                  );
-                }),
-          ),
-        ));
+                    Text(
+                      userData.userName,
+                      style: const TextStyle(),
+                    ),
+                    MarginSizedBox.smallHeightMargin,
+                    Text(myUserEmail ?? ''),
+                    MarginSizedBox.smallHeightMargin,
+                    Text(userData.profileIntroduction),
+                    MarginSizedBox.bigHeightMargin,
+                  ],
+                );
+              }),
+        ),
+      ),
+      drawer: StreamBuilder<Object>(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .doc(myUserId)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const SizedBox.shrink();
+            }
+            if (snapshot.hasData == false) {
+              return const SizedBox.shrink();
+            }
+            final DocumentSnapshot<Map<String, dynamic>>? documentSnapshot =
+                snapshot.data as DocumentSnapshot<Map<String, dynamic>>?;
+            if (documentSnapshot == null || !documentSnapshot.exists) {
+              // ドキュメントが存在しない場合の処理
+              return const SizedBox.shrink();
+            }
+            final Map<String, dynamic> userDataMap = documentSnapshot.data()!;
+            final UserData userData = UserData.fromJson(userDataMap);
+            return Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  MarginSizedBox.bigHeightMargin,
+                  ListTile(
+                    title: const Text('メールアドレス変更'),
+                    onTap: () {
+                      Navigator.pop(context); // Drawerを閉じる
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const EditEmailPage()),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    title: const Text('パスワード変更'),
+                    onTap: () async {
+                      showConfirmDialog(
+                        context: context,
+                        onPressed: () async {
+                          try {
+                            await FirebaseAuth.instance
+                                .sendPasswordResetEmail(email: myUserEmail!);
+                            showToast("パスワードリセット用のメールを送信しました");
+                            // ignore: use_build_context_synchronously
+                            Navigator.pop(context);
+                          } catch (e) {
+                            showCloseOnlyDialog(
+                                context, e.toString(), 'メール送信失敗');
+                          }
+                        },
+                        text: 'パスワード再設定メールを送信しますか？',
+                      );
+                    },
+                  ),
+                  ListTile(
+                    title: const Text('プロフィール編集'),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) {
+                          return EditProfilePage(
+                            userName: userData.userName,
+                            imageUrl: userData.imageUrl,
+                          );
+                        }),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    title: const Text('ログアウト'),
+                    onTap: () async {
+                      await FirebaseAuth.instance.signOut();
+                      print('ログアウトしました');
+                    },
+                  ),
+                ],
+              ),
+            );
+          }),
+    );
   }
 }
